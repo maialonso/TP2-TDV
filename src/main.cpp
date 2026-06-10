e84a57a4d9ebb47e28171e0b82b7f7e1ab00e105
maialonso <maia.alonso2005@gmail.com>
2026-06-10 16:01:46 -0300

    Heurística 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,100 @@
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <climits>
+#include "Instancia.h"
+#include "Solucion.h"
+
+Solucion heuristica1(const Instancia& instancia) {
+
+    int n = instancia.cantidadVendedores();
+    int m = instancia.cantidadDepositos();
+
+    Solucion solucion(n);
+
+    std::vector<int> capacidades =
+        instancia.capacidades();
+
+    for(int vendedor = 0; vendedor < n; vendedor++) {
+
+        int mejorDeposito = -1;
+        double mejorCosto = INT_MAX;
+
+        for(int deposito = 0; deposito < m; deposito++) {
+
+            int demanda =
+                instancia.demanda(deposito, vendedor);
+
+            if(capacidades[deposito] >= demanda) {
+
+                double costo =
+                    instancia.costo(deposito, vendedor);
+
+                if(costo < mejorCosto) {
+
+                    mejorCosto = costo;
+                    mejorDeposito = deposito;
+                }
+            }
+        }
+
+        if(mejorDeposito != -1) {
+
+            solucion.asignar(vendedor,
+                             mejorDeposito);
+
+            capacidades[mejorDeposito] -=
+                instancia.demanda(mejorDeposito,
+                                  vendedor);
+        }
+    }
+
+    return solucion;
+}
 
 
-int main(int argc, char** argv) {
-    std::string filename = "instances/gap/gap_a/a05100";
-    std::cout << "Reading file " << filename << std::endl;
 
-    // Aca empieza la magia.
+int main() {
+
+    Instancia instancia("instances/gap/gap_b/b05200");
+
+    std::cout << instancia.cantidadDepositos() << std::endl;
+    std::cout << instancia.cantidadVendedores() << std::endl;
+
+    Solucion s = heuristica1(instancia);
+
+    double suma = 0;
+    int sinAsignar = 0;
+
+    for(int v = 0; v < instancia.cantidadVendedores(); v++) {
+
+        int d = s.depositoDe(v);
+
+        if(d == -1) {
+            std::cout << "Vendedor sin asignar: "
+                      << v << std::endl;
+            sinAsignar++;
+            continue;
+        }
+
+        std::cout << "vendedor "
+                  << v
+                  << " -> deposito "
+                  << d
+                  << std::endl;
+
+        suma += instancia.costo(d, v);
+    }
+
+    std::cout << "Costo total: "
+              << suma
+              << std::endl;
+
+    std::cout << "Sin asignar: "
+              << sinAsignar
+              << std::endl;
 
     return 0;
 }
